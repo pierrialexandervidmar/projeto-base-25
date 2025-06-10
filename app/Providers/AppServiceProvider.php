7a0f75091509541454b76a55196a2a2b4439c811
pierrialexandervidmar <pierri@hotmail.com.br>
2025-06-10 19:18:08 -0300

    chore: comando do AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Opcodes\LogViewer\Facades\LogViewer;
 
@@ -24,10 +26,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->setupLogViewer();
+        $this->configModels();
+        $this->configCommands();
     }
 
     public function setupLogViewer(): void
     {
         LogViewer::auth(fn ($request) => $request->user()->is_admin);
+    }
+
+    public function configModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+
+    private function configCommands() 
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
     }
 }
